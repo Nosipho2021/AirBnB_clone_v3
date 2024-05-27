09dcd7984e37ef1ceacb8d016a31d8d6a3612d84
Nosipho2021 <lorrainemdk@gmail.com>
2024-05-27 12:35:44 -0700

    Add stats endpoint to retrieve count of each object type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,25 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+     """
+    Retrieves the number of each objects by type.
     """
-    Returns the status of the API.
-    """
-    return jsonify({"status": "OK"})
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(stats)
