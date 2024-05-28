35431b9b4a88ac44d6bffe7b2190ee321614a55d
Nosipho2021 <lorrainemdk@gmail.com>
2024-05-27 17:20:55 -0700

    update

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains the TestAmenityDocs classes
+"""
+
+from datetime import datetime
+import inspect
+import models
+from models import amenity
+from models.base_model import BaseModel
+import pep8
+import unittest
+Amenity = amenity.Amenity
